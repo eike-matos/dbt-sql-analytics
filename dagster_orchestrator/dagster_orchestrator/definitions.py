1fc0dc70e9df88c4aade28f6098d31a7ef80cfcd
Eike-Matos <eikeadrian9830@gmail.com>
2026-08-10 15:47:31 -0300

    [#EM-010] creating of job an schedule based on codebase movies

--- dagster_orchestrator/dagster_orchestrator/definitions.py
+++ dagster_orchestrator/dagster_orchestrator/definitions.py
@@ -4,6 +4,8 @@ from dagster import Definitions, load_assets_from_modules, EnvVar
 
 from .assets import mongodb, movies
 from .resources import snowflake_resource, dlt_resource
+from .jobs import movies_job
+from .schedules import movies_schedule
 
 mongodb_assets = load_assets_from_modules([mongodb])
 movies_assets = load_assets_from_modules([movies], group_name="movies")
@@ -14,5 +16,7 @@ defs = Definitions(
     resources={
         "dlt": dlt_resource,
         "snowflake": snowflake_resource
-    }
+    },
+    jobs=[movies_job],
+    schedules=[movies_schedule]
 )
