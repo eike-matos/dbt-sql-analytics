c89448d4d3b2a5f9a30986fe49da58cb5aedeb14
Eike-Matos <eikeadrian9830@gmail.com>
2026-08-10 13:58:57 -0300

    [#EM-008] refactor of codebase and testing run the connection of dagster locally

--- dagster_orchestrator/dagster_orchestrator/definitions.py
+++ dagster_orchestrator/dagster_orchestrator/definitions.py
@@ -1,9 +1,13 @@
 from dagster import Definitions, load_assets_from_modules
+from dagster_embedded_elt.dlt import DagsterDltResource
 
-from dagster_orchestrator import assets  # noqa: TID252
+from .assets import mongodb
 
-all_assets = load_assets_from_modules([assets])
+mongodb_assets = load_assets_from_modules([mongodb])
 
 defs = Definitions(
-    assets=all_assets,
+    assets=[*mongodb_assets],
+    resources={
+        "dlt": DagsterDltResource()
+    }
 )
