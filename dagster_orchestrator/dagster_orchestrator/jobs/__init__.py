09b548ffebf346cc2904528bd4ff6c0ba7e3515f
Eike-Matos <eikeadrian9830@gmail.com>
2026-08-10 16:24:47 -0300

    [#EM-012] define adhoc sensors and implementation the codebase to deal with it

--- dagster_orchestrator/dagster_orchestrator/jobs/__init__.py
+++ dagster_orchestrator/dagster_orchestrator/jobs/__init__.py
@@ -4,3 +4,8 @@ movies_job = define_asset_job(
     name="movies_job",
     selection=AssetSelection.all() - AssetSelection.groups("mongodb")
 )
+
+adhoc_job = define_asset_job(
+    name="adhoc_job",
+    selection=AssetSelection.assets(["movie_embeddings"])
+)
